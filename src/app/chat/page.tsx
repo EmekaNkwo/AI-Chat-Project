46289bbc456fe283b04ee0e7d60b149e430d0520
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2024-10-15 22:02:05 +0100

    Fixing deployment issues

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,9 @@
-import ChatApp from "@/component/ChatApp";
-import React from "react";
+import dynamic from "next/dynamic";
 
+// import ChatApp from "@/component/ChatApp";
+const ChatApp = dynamic(() => import("@/component/ChatApp"), {
+  ssr: false,
+});
 const Page = () => {
   return (
     <div>
